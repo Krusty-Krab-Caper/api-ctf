457df0f4c174e37d5cb6e0d90ec4c3659a58157d
Josh Hendershot <hendershotjoshr@gmail.com>
2024-05-03 11:47:27 -0600

    feat: register chapter 2

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-
 import fastify from 'fastify'
-import { registerChapter1 } from './chapter1';
-import { registerSecrets } from './gameSecrets';
-import { ErrorResponse } from './util';
+import { registerChapter1 } from './chapter1'
+import { registerSecrets } from './gameSecrets'
+import { registerChapter2 } from './chapter2'
+import { ErrorResponse } from './util'
 
-const server = fastify();
+const server = fastify()
 
 registerChapter1(server)
+registerChapter2(server)
 
 registerSecrets(server)
 
 server.setNotFoundHandler(async function (request, reply) {
-    await reply.code(404).send(ErrorResponse(404, 'Path not found'))
-  })
-  server.setErrorHandler(async function (error, request, reply) {
-    if (error.statusCode === 400) {
-        server.log.warn({ err: error }, 'Invalid request error')
-      await reply.code(400).send(ErrorResponse(400, error.message))
-    } else if (error.statusCode === 404) {
-      await reply.code(404).send(ErrorResponse(404, error.message))
-    } else {
-        server.log.error({ err: error }, 'Internal Server error')
-      await reply.code(error.statusCode ?? 500).send(ErrorResponse(500, error.message))
-    }
-  })
-
-server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
+  await reply.code(404).send(ErrorResponse(404, 'Path not found'))
+})
+server.setErrorHandler(async function (error, request, reply) {
+  if (error.statusCode === 400) {
+    server.log.warn({ err: error }, 'Invalid request error')
+    await reply.code(400).send(ErrorResponse(400, error.message))
+  } else if (error.statusCode === 404) {
+    await reply.code(404).send(ErrorResponse(404, error.message))
+  } else {
+    server.log.error({ err: error }, 'Internal Server error')
+    await reply.code(error.statusCode ?? 500).send(ErrorResponse(500, error.message))
+  }
 })
 
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
