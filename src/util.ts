6f68520e2ef0d4d2c2e3fd065e774aa3e6290140
Josh Hendershot <hendershotjoshr@gmail.com>
2024-05-06 14:48:21 -0600

    feat: randomToken() function to keep things consistent

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,18 @@
 import * as crypto from 'crypto'
 
 export function sha1(input: string): string {
-    const hash = crypto.createHash('sha1');
-    hash.update(input);
-    return hash.digest('hex');
+  const hash = crypto.createHash('sha1')
+  hash.update(input)
+  return hash.digest('hex')
 }
 
-export function ErrorResponse (code: number, message: string){
-    return {
-        code,
-        message
-    }
+export function randomToken(): string {
+  return crypto.randomBytes(32).toString('hex')
+}
+
+export function ErrorResponse(code: number, message: string) {
+  return {
+    code,
+    message
+  }
 }
