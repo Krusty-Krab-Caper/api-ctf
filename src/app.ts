60886a18df463fca1b67729d57a451d95c40bef9
Josh Hendershot <hendershotjoshr@gmail.com>
2024-05-07 09:49:14 -0600

    refactor: extract endpoint registration from server listening

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import fastifyFormbody from '@fastify/formbody'
+import { registerDirectory } from './directory'
+import { registerClients } from './clientsAdmin'
+import { registerSecrets } from './gameSecrets'
+import { ErrorResponse } from './util'
+import { registerMitm } from './mitm'
+import { registerVault } from './vault'
+import { registerToken } from './tokenEndpoint'
+import fastify from 'fastify'
+
+export function build() {
+  const server = fastify()
+  server.register(fastifyFormbody)
+
+  registerDirectory(server)
+  registerClients(server)
+  registerMitm(server)
+  registerVault(server)
+  registerToken(server)
+  registerSecrets(server)
+
+  server.setNotFoundHandler(async function (request, reply) {
+    await reply.code(404).send(ErrorResponse(404, 'Path not found'))
+  })
+
+  server.setErrorHandler(async function (error, request, reply) {
+    if (error.statusCode === 400) {
+      server.log.warn({ err: error }, 'Invalid request error')
+      await reply.code(400).send(ErrorResponse(400, error.message))
+    } else if (error.statusCode === 404) {
+      await reply.code(404).send(ErrorResponse(404, error.message))
+    } else {
+      server.log.error({ err: error }, 'Internal Server error')
+      await reply.code(error.statusCode ?? 500).send(ErrorResponse(500, error.message))
+    }
+  })
+
+  return server
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,6 @@
-import fastify from 'fastify'
-import fastifyFormbody from '@fastify/formbody'
+import { build } from './app'
 
-import { registerDirectory } from './directory'
-import { registerClients } from './clientsAdmin'
-import { registerSecrets } from './gameSecrets'
-import { ErrorResponse } from './util'
-import { getSecrets } from './gameSecrets'
-import { registerMitm } from './mitm'
-import { registerVault } from './vault'
-import { registerToken } from './tokenEndpoint'
-
-const server = fastify()
-
-server.register(fastifyFormbody)
-
-registerDirectory(server)
-registerClients(server)
-registerMitm(server)
-registerVault(server)
-registerToken(server)
-
-registerSecrets(server)
-
-server.setNotFoundHandler(async function (request, reply) {
-  await reply.code(404).send(ErrorResponse(404, 'Path not found'))
-})
-server.setErrorHandler(async function (error, request, reply) {
-  if (error.statusCode === 400) {
-    server.log.warn({ err: error }, 'Invalid request error')
-    await reply.code(400).send(ErrorResponse(400, error.message))
-  } else if (error.statusCode === 404) {
-    await reply.code(404).send(ErrorResponse(404, error.message))
-  } else {
-    server.log.error({ err: error }, 'Internal Server error')
-    await reply.code(error.statusCode ?? 500).send(ErrorResponse(500, error.message))
-  }
-})
+const server = build()
 
 server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
